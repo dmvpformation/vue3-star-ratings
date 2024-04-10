a4d4e223ac9897b59f17d414a434d6d82c174ba8
Levy NALY <l.naly@maformationofficinale.com>
2024-04-10 09:26:57 +0200

    feat: update build and version

--- dist/vue3-star-ratings.esm.js
+++ dist/vue3-star-ratings.esm.js
@@ -152,9 +152,9 @@ var script = defineComponent({
 
 });
 
-const _withId = /*#__PURE__*/withScopeId("data-v-fc78e576");
+const _withId = /*#__PURE__*/withScopeId("data-v-507adcfc");
 
-pushScopeId("data-v-fc78e576");
+pushScopeId("data-v-507adcfc");
 
 const _hoisted_1 = {
   class: "vue3-star-ratings__wrapper"
@@ -259,11 +259,11 @@ function styleInject(css, ref) {
   }
 }
 
-var css_248z = "\n.vue3-star-ratings__wrapper[data-v-fc78e576] {\r\n  display: block;\r\n  margin: 25px auto;\r\n  text-align: center;\r\n  padding: 25px;\n}\n.vue3-star-ratings[data-v-fc78e576] {\r\n  display: flex;\r\n  justify-content: space-between;\r\n  align-items: center;\n}\n.vue3-star-ratings button[data-v-fc78e576] {\r\n  border: 0;\r\n  display: flex;\r\n  justify-content: center;\r\n  align-items: center;\r\n  border-radius: 50%;\r\n  cursor: pointer;\n}\n.vue3-star-ratings button svg[data-v-fc78e576] {\r\n  fill: currentColor;\n}\n.stars[data-v-fc78e576] {\r\n  display: inline-block;\r\n  position: relative;\r\n  overflow: hidden;\r\n  margin: 0 auto;\n}\n.stars-outer[data-v-fc78e576],\r\n.stars-inner[data-v-fc78e576] {\r\n  height: inherit;\n}\n.stars-inner[data-v-fc78e576] {\r\n  position: absolute;\r\n  top: 0;\r\n  left: 0;\r\n  width: 0;\r\n  max-width: 100%;\r\n  min-width: 0;\r\n  white-space: nowrap;\r\n  transition: 320ms cubic-bezier(0.075, 0.82, 0.165, 1);\r\n  overflow: hidden;\n}\n.stars svg[data-v-fc78e576] {\r\n  display: inline-block;\r\n  fill: currentColor;\r\n  cursor: pointer;\n}\r\n";
+var css_248z = "\n.vue3-star-ratings__wrapper[data-v-507adcfc] {\n  display: block;\n  text-align: center;\n}\n.vue3-star-ratings[data-v-507adcfc] {\n  display: flex;\n  justify-content: space-between;\n  align-items: center;\n}\n.vue3-star-ratings button[data-v-507adcfc] {\n  border: 0;\n  display: flex;\n  justify-content: center;\n  align-items: center;\n  border-radius: 50%;\n  cursor: pointer;\n}\n.vue3-star-ratings button svg[data-v-507adcfc] {\n  fill: currentColor;\n}\n.stars[data-v-507adcfc] {\n  display: inline-block;\n  position: relative;\n  overflow: hidden;\n  margin: 0 auto;\n}\n.stars-outer[data-v-507adcfc],\n.stars-inner[data-v-507adcfc] {\n  height: inherit;\n}\n.stars-inner[data-v-507adcfc] {\n  position: absolute;\n  top: 0;\n  left: 0;\n  width: 0;\n  max-width: 100%;\n  min-width: 0;\n  white-space: nowrap;\n  transition: 320ms cubic-bezier(0.075, 0.82, 0.165, 1);\n  overflow: hidden;\n}\n.stars svg[data-v-507adcfc] {\n  display: inline-block;\n  fill: currentColor;\n  cursor: pointer;\n}\n";
 styleInject(css_248z);
 
 script.render = render;
-script.__scopeId = "data-v-fc78e576";
+script.__scopeId = "data-v-507adcfc";
 
 // Import vue component
 
